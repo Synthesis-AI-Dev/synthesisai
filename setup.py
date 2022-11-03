38b1a014242ece1cbb9142a765621d0241ef4f2b
Anish Chopra <anish@synthesis.ai>
2022-11-03 15:15:35 -0700

    Update setup.py to add requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+with open("requirements.txt") as requirements_file:
+    requirements = requirements_file.read().splitlines()
 
 test_requirements = [ ]
 
