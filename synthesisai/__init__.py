57e1fe47bfd18ef8754f38ca016429d6953deeac
Anish Chopra <anish@synthesis.ai>
2022-11-03 15:30:49 -0700

    Update version to 0.1.2

--- synthesisai/__init__.py
+++ synthesisai/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Synthesis AI"""
 __email__ = 'anish@synthesis.ai'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from .dataset import Grouping, SaiDataset
 from .modality import Modality
