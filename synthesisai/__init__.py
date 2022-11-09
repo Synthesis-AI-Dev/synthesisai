846d5518b70a589ceea3deac2a40c5dd04f94833
Anish Chopra <anish@synthesis.ai>
2022-11-09 01:34:27 -0500

    Bump version: 0.1.3 → 0.1.4

--- synthesisai/__init__.py
+++ synthesisai/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Synthesis AI"""
 __email__ = 'anish@synthesis.ai'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from .dataset import Grouping, SaiDataset
 from .modality import Modality
